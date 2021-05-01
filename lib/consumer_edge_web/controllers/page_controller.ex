8da8c9577c0455b7648ef19a79cb7facc25c9ba1
Jake Romer <jake@jmromer.org>
2021-05-01 16:15:41 -0400

    Add login, signup, landing pages layout
    Adds base styling and markup, assets.
    
    CLOCK-IN: Sat, 1 May 2021 16:15:41 -0400
    CLOCK-OUT: Sat, 1 May 2021 18:04:54 -0400
    
    CLOCK-SECS: 6553
    TOTAL-SECS: 6553
    
    CLOCK-ELAPSED: 01:49:13
    TOTAL-ELAPSED: 01:49:13

--- lib/consumer_edge_web/controllers/page_controller.ex
+++ lib/consumer_edge_web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule ConsumerEdgeWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def login(conn, _params) do
+    render(conn, "login.html")
+  end
+
+  def signup(conn, _params) do
+    render(conn, "signup.html")
+  end
 end
--- lib/consumer_edge_web/router.ex
+++ lib/consumer_edge_web/router.ex
@@ -17,6 +17,8 @@ defmodule ConsumerEdgeWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/login", PageController, :login
+    get "/signup", PageController, :signup
   end
 
   # Other scopes may use custom stacks.
