732256ebea8a4e07382448ffef91d5cfde7b7e8c
Jake Romer <jake@jmromer.org>
2021-05-02 08:59:49 -0400

    Cache API requests
    CLOCK-IN: Sun, 2 May 2021 08:59:49 -0400
    CLOCK-OUT: Sun, 2 May 2021 08:59:54 -0400
    
    CLOCK-SECS: 5
    TOTAL-SECS: 13723
    
    CLOCK-ELAPSED: 00:00:05
    TOTAL-ELAPSED: 03:48:43

--- lib/consumer_edge/marketstack.ex
+++ lib/consumer_edge/marketstack.ex
@@ -1,14 +1,18 @@
 defmodule Marketstack do
+  use Nebulex.Caching
+  @ttl :timer.minutes(10)
+
   @base_url "http://api.marketstack.com/v1"
   @access_key System.get_env("MARKETSTACK_API_KEY")
 
-  def eod(ticker) do
+  @decorate cacheable(cache: ConsumerEdge.Cache, key: {:eod, ticker}, opts: [ttl: @ttl])
+  def end_of_day(ticker) do
     url = @base_url <> "/eod?access_key=" <> @access_key <> "&symbols=" <> ticker
 
     case HTTPoison.get(url) do
       {:error, %{reason: reason}} ->
-        {:error, reason}
-      {:ok, %{body: raw_body, status_code: code}} ->
+        %{error: reason}
+      {:ok, %{body: raw_body}} ->
         raw_body
         |> Poison.decode!
         |> Map.get("data")
--- lib/consumer_edge_web/controllers/page_controller.ex
+++ lib/consumer_edge_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule ConsumerEdgeWeb.PageController do
   use ConsumerEdgeWeb, :controller
 
   def index(conn, _params) do
-    data = Poison.encode!(Marketstack.eod("aapl"))
+    data = Poison.encode!(Marketstack.end_of_day("aapl"))
     render(conn, "index.html", json: data)
   end
 end
