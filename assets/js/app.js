d332d346adb50c964fce227173d4983580905d76
Jake Romer <jake@jmromer.org>
2021-05-01 23:01:32 -0400

    Make background image rendering more robust
    CLOCK-IN: Sat, 1 May 2021 22:30:00 -0400
    CLOCK-OUT: Sat, 1 May 2021 23:00:00 -0400
    
    CLOCK-SECS: 1800
    TOTAL-SECS: 10807
    
    CLOCK-ELAPSED: 00:30:00
    TOTAL-ELAPSED: 03:00:07

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import '../css/app.scss'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,4 +12,21 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import 'phoenix_html'
+
+// Update body background size and position dynamically based on
+// orientation and aspect ratio. An expedient alternative to a Canvas-rendered
+// background.
+const body = document.querySelector('body')
+const bgPositions = ['center', 'left', 'right', 'top']
+const mediaQuery = 'only screen and (min-aspect-ratio: 5/4)'
+
+window.matchMedia(mediaQuery).addListener(mql => {
+  if (mql.matches) {
+    body.style.backgroundPosition = 'unset'
+    body.style.backgroundSize = 'cover'
+  } else {
+    body.style.backgroundPosition = bgPositions[Math.floor(Math.random() * 4)]
+    body.style.backgroundSize = 'auto'
+  }
+})
